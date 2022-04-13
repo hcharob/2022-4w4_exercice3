bdcaa83e92383f83b38c2483e0ffa9962c827681
Hervée Charbonneau-Robichaud <90909822+hcharob@users.noreply.github.com>
2022-04-12 21:28:21 -0400

    s11 c1 hc - Ajustements des informations du footer pour le mode mobile

--- footer.php
+++ footer.php
@@ -17,18 +17,20 @@
     <div class="site__footer__ligne">
         <section class="footer__description">
             <section class="footer__nav"> </section>
-            <section class="footer__sociaux">
-                <?php get_sidebar('footer_ligne_1'); ?> 
-            </section>
+            <?php get_search_form() ?>
+            
             <section class="copyright"> </section>
             <section class="footer__auteur"> </section>
         </section>
+        
     </div>
 
     <h2 class="footer__titre">3800 Rue Sherbrooke E, Montréal, QC H1X 2A2</h2>
     <h3 class="footer__author">Fait par Hervée Charbonneau-Robichaud</h3>
     <p class="footer__presentation">© Tous droits réservés</p>
-    <?php get_search_form() ?>
+    <section class="footer__sociaux">
+                <?php get_sidebar('footer_ligne_1'); ?> 
+            </section>
     <?php 
 
 $icone = '<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" color="#000"><path d="M0 0h24v24H0z" fill="none"></path><path d="M8 5v14l11-7z"></path></svg>';
