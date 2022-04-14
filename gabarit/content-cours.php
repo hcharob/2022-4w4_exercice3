d34055a9548957e395dae26f7df03ad73a5dd13b
Hervée Charbonneau-Robichaud <rvcharob@gmail.com>
2022-04-14 16:26:57 -0400

    s11 c2 hc - Ajout du groupe de champs événement

--- gabarit/content-cours.php
+++ gabarit/content-cours.php
@@ -5,11 +5,12 @@
 <article class="formation__cours <?php echo $categories[1]->slug; ?>">
     <?php
             $titre = get_the_title();
-            $titreFiltreCours = substr($titre, 4, -6);
+            $titreFiltreCours = substr($titre, 3, -6);
             // $nbHeures = substr($titre, -6);
             $nbHeures = get_field("nombre_dheures");
-            $sigleCours = substr($titre, 0, 3);
-            $descCours = get_the_excerpt();
+            $departement = get_field("departement");
+            $sigleCours = substr($titre, 0, 4);
+            $descCours = wp_trim_words(get_the_content(), 15, "<a href='#'> La suite </a>");
             ?>
     <?php the_post_thumbnail("thumbnail"); ?>
 
@@ -22,4 +23,5 @@
     <div class="cours__nbre-heure"><?= $nbHeures . "h"; ?></div>
     <p class="cours__sigle"><?= $sigleCours; ?> </p>
     <p class="cours__desc"> <?= $descCours; ?></p>
+    <p class="cours__departement"><?= $departement; ?></p>
 </article>
--- template-evenement.php
+++ template-evenement.php
@@ -10,12 +10,26 @@
 
 <?php get_header() ?>
 <main class="site__main">
+   <article class="evenement">
 
-   <!-- <h1> --------- Template Evenement --------- </h1> -->
-   <?php if (have_posts()): the_post(); ?>
-   <?php the_title() ?>
-   <?php the_content() ?>
+
+      <!-- <h1> --------- Template Evenement --------- </h1> -->
+      <?php if (have_posts()): the_post(); ?>
+      <h1><?php the_title() ?></h1>
+      <section class="evenement__resume">
+         <?php the_field('resume'); ?>
+      </section>
+
+      <p class="evenement__endroit">
+      <?php the_field('endroit'); ?>
+      </p>
+
+   </article>
    <?php endif ?>
-   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#3D8BF2" fill-opacity="1" d="M0,128L0,32L130.9,32L130.9,256L261.8,256L261.8,128L392.7,128L392.7,32L523.6,32L523.6,288L654.5,288L654.5,32L785.5,32L785.5,128L916.4,128L916.4,32L1047.3,32L1047.3,256L1178.2,256L1178.2,128L1309.1,128L1309.1,224L1440,224L1440,0L1309.1,0L1309.1,0L1178.2,0L1178.2,0L1047.3,0L1047.3,0L916.4,0L916.4,0L785.5,0L785.5,0L654.5,0L654.5,0L523.6,0L523.6,0L392.7,0L392.7,0L261.8,0L261.8,0L130.9,0L130.9,0L0,0L0,0Z"></path></svg>
+   <!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path fill="#3D8BF2" fill-opacity="1"
+         d="M0,128L0,32L130.9,32L130.9,256L261.8,256L261.8,128L392.7,128L392.7,32L523.6,32L523.6,288L654.5,288L654.5,32L785.5,32L785.5,128L916.4,128L916.4,32L1047.3,32L1047.3,256L1178.2,256L1178.2,128L1309.1,128L1309.1,224L1440,224L1440,0L1309.1,0L1309.1,0L1178.2,0L1178.2,0L1047.3,0L1047.3,0L916.4,0L916.4,0L785.5,0L785.5,0L654.5,0L654.5,0L523.6,0L523.6,0L392.7,0L392.7,0L261.8,0L261.8,0L130.9,0L130.9,0L0,0L0,0Z">
+      </path>
+   </svg> -->
 </main>
 <?php get_footer() ?>
