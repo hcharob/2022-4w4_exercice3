dc158eb3ea1c86ad54f6917d2bbe45aa4569cc52
Hervée Charbonneau-Robichaud <rvcharob@gmail.com>
2022-04-21 15:38:15 -0400

    s12 c1 hc - 4e commit de la branche exer4, ajout d'une enqueue scripts

--- functions.php
+++ functions.php
@@ -1,7 +1,16 @@
 <?php function cidw_4w4_enqueue(){
     //wp_enqueue_style('style_css', get_stylesheet_uri());
-    wp_enqueue_style('4w4-le-style', get_template_directory_uri() . '/style.css', array(), filemtime(get_template_directory() . '/style.css'), false);
+    wp_enqueue_style('4w4-le-style',
+                        get_template_directory_uri() . '/style.css',
+                        array(), filemtime(get_template_directory() . '/style.css'),
+                        false);
+
     wp_enqueue_style('cidw-4w4-google-font', "https://fonts.googleapis.com/css2?family=Lobster&family=Montserrat&family=Poppins&family=Roboto&display=swap", false);
+
+    wp_enqueue_script('cidw-4w4-boite-modale', 
+                        get_template_directory_uri() . '/javascript/boite-modale.js', //dossier contenant le thème
+                        array(), filemtime(get_template_directory() . '/javascript/boite-modale.js'), 
+                        true); //true pour intégrer le js en bas du document
 }
 
 add_action("wp_enqueue_scripts", "cidw_4w4_enqueue");
---
+++ javascript/boite-modale.js
@@ -0,0 +1,3 @@
+(function(){
+    console.log("vive la boite modale...");
+})()
