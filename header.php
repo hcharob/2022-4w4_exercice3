81ebd5af55b7286b9075a948a1f8ed6eb95d2cb7
Hervée Charbonneau-Robichaud <rvcharob@gmail.com>
2022-04-25 11:01:27 -0400

    s13 c1 hc - Ajustement a functions php pour faire fonctionner apparence body

--- header.php
+++ header.php
@@ -15,34 +15,36 @@
     <link href="https://fonts.googleapis.com/css2?family=Lemon&family=Montserrat&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;1,800;1,900&display=swap"
         rel="stylesheet">
-
     <?php wp_head() ?>
     <?php show_admin_bar(true); ?>
 </head>
 
-<body <?php body_class("site"); ?>>
+<body <?php body_class("site"); ?> style="background-color:<?= get_theme_mod("background_body"); ?>;" >
     <header class="site__header">
+        <section class="site__header__titre">
+            <?=get_custom_logo(); ?>
+            <h1 class="header__titre">
+                <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                    <?php bloginfo('name'); ?></h1>
+            </a>
+            <h2 class="header__description"><?php bloginfo('description'); ?></h2>
+        </section>
 
 
-<section class="site__header__titre">
-<?= get_custom_logo(); ?>
-        
-        <h1 class="header__titre">
-            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-                <?php bloginfo('name'); ?></h1>
-        </a>
-
-        <h2 class="header__description"><?php bloginfo('description'); ?></h2>
-</section>
         <section class="util">
-        <?= get_sidebar('entete_1'); ?>
+            <?= get_sidebar('entete_1'); ?>
         </section>
     </header>
 
     <section class="site__barre">
         <input type="checkbox" id="chk-burger">
         <label for="chk-burger" id="burger">
-        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#000"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+            <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                color="#000">
+                <path fill-rule="evenodd"
+                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                    clip-rule="evenodd"></path>
+            </svg>
         </label>
         <?php wp_nav_menu(array("menu"=>"principal",
                             "container"=>"nav")); ?>
