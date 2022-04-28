1ade23ad3abeb05b0cf93efacb59151f6843afc8
Hervée Charbonneau-Robichaud <rvcharob@gmail.com>
2022-04-28 16:56:22 -0400

    s13 c2 hc - Création d'une animation pour le bouton burger avec l'aide du clip path

--- header.php
+++ header.php
@@ -21,9 +21,9 @@
             background-color: <?= get_theme_mod("background_clippath"); ?>;
         }
 
-        .site__main {
-            
-        }
+        /* .site__main {
+
+        } */
     </style>
     <?php show_admin_bar(true); ?>
 </head>
@@ -47,13 +47,10 @@
 
     <section class="site__barre">
         <input type="checkbox" id="chk-burger">
-        <label for="chk-burger" id="burger">
-            <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                color="#000">
-                <path fill-rule="evenodd"
-                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                    clip-rule="evenodd"></path>
-            </svg>
+        <label for="chk-burger" id="burger" class="burger">
+           <div class="burger__ligne"> </div>
+           <div class="burger__ligne"> </div>
+           <div class="burger__ligne"> </div>
         </label>
         <?php wp_nav_menu(array("menu"=>"principal",
                             "container"=>"nav")); ?>
