6a1582df8106e6772ce2f3cd654d817aabf33d34
Hervée Charbonneau-Robichaud <90909822+hcharob@users.noreply.github.com>
2022-05-13 18:19:52 -0400

    15 c2 hc - Ajustement au curseur pour qu'il soit au milieu de la souris

--- javascript/curseur.js
+++ javascript/curseur.js
@@ -1,15 +1,20 @@
-(function(){
-        //L'élément HTML qui agira comme curseur
-        let leCurseur = document.querySelector(".curseur");
-        window.addEventListener("mousemove", deplacerCurseur);
+ //L'élément HTML qui agira comme curseur
+ let leCurseur = document.querySelector(".curseur");
 
-        /**
-         * Fonction permettant de déplacer le curseur à l'endroit du
-         * pointeur de la souris dans l'écran
-         * 
-         * @param {object} event Informations sur l'événement MouseEvent distribué
-         */
-        function deplacerCurseur(event) {
-            leCurseur.style.transform = `translate(${event.clientX}px, ${event.clientY}px)`;
-        }
-})()
+ //Écouteur sur la fenêtre pour l'événement de souris "mousemove"
+ window.addEventListener("mousemove", deplacerCurseur);
+
+
+ /**
+  * Fonction permettant de déplacer le curseur à l'endroit du
+  * pointeur de la souris dans l'écran
+  * 
+  * @param {object} event Informations sur l'événement MouseEvent distribué
+  */
+ function deplacerCurseur(event) {
+     //Coordonnées X et Y du pointeur de la souris
+     //console.log("client X, Y : ", event.clientX, event.clientY);
+
+     //Déplacer le curseur à l'endroit du pointeur de la souris
+     leCurseur.style.transform = `translate(${event.clientX}px, ${event.clientY}px)`;
+ }
